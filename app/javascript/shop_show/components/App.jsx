502350fb0cfbf68a98e41feb166eb02802bb6ebe
Scott Crawford <scrawford@digitalocean.com>
2018-08-20 21:38:45 -0400

    add a carousel to the show page

--- app/javascript/shop_show/components/App.jsx
+++ app/javascript/shop_show/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import NavBar from '../../shared/components/nav_bar'
+import Carousel from 'nuka-carousel';
 
 export default class App extends React.Component {
   state = {
@@ -15,6 +16,15 @@ export default class App extends React.Component {
         <NavBar/>
         <div className="body_content">
           This is the single car page
+
+          <Carousel>
+            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1" />
+            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2" />
+            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3" />
+            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4" />
+            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5" />
+            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6" />
+          </Carousel>
         </div>
       </div>
     )
