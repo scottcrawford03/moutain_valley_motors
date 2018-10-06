c120cd4eeba0b9c622d87372d3567cfab478088a
Scott Crawford <scrawford@digitalocean.com>
2018-10-05 21:44:20 -0400

    use raptor photos

--- app/javascript/homepage/components/homepage.jsx
+++ app/javascript/homepage/components/homepage.jsx
@@ -16,12 +16,12 @@ export default class App extends React.Component {
         <Carousel
           height={'100%'}
         >
-          <img src="https://www.nps.gov/yose/planyourvisit/images/20170618_155330.jpg?maxwidth=1200&maxheight=1200&autorotate=false" />
-          <img src="https://www.myyosemitepark.com/.image/t_share/MTQ3OTg4NjE5MzY2NjM5MDcx/yosemite-glacier-point.jpg" />
-          <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3" />
-          <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4" />
-          <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5" />
-          <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6" />
+          <img src="https://mountainvalleymotors.nyc3.digitaloceanspaces.com/abc123-ford-f150-raptor/front.jpg" />
+          <img src="https://mountainvalleymotors.nyc3.digitaloceanspaces.com/abc123-ford-f150-raptor/front.jpg" />
+          <img src="https://mountainvalleymotors.nyc3.digitaloceanspaces.com/abc123-ford-f150-raptor/front.jpg" />
+          <img src="https://mountainvalleymotors.nyc3.digitaloceanspaces.com/abc123-ford-f150-raptor/front.jpg" />
+          <img src="https://mountainvalleymotors.nyc3.digitaloceanspaces.com/abc123-ford-f150-raptor/front.jpg" />
+          <img src="https://mountainvalleymotors.nyc3.digitaloceanspaces.com/abc123-ford-f150-raptor/front.jpg" />
         </Carousel>
       </Application>
     )
